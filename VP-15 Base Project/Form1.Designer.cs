ab34796ac51349d06315dea65ea32eb6ae4925db
les jackson <les.jackson@binarythistle.com>
2018-02-13 19:36:03 +1100

    initial commit

---
+++ VP-15 Base Project/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace VP_15_Base_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.cmdGO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboVerb = new System.Windows.Forms.ComboBox();
+            this.txtPOSTData = new System.Windows.Forms.TextBox();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.cmdCopy = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboURIs = new System.Windows.Forms.ComboBox();
+            this.txtAuthToken = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtClientSecret = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtClientID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResponse.Location = new System.Drawing.Point(91, 225);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(537, 83);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // cmdGO
+            // 
+            this.cmdGO.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdGO.Location = new System.Drawing.Point(553, 10);
+            this.cmdGO.Name = "cmdGO";
+            this.cmdGO.Size = new System.Drawing.Size(75, 23);
+            this.cmdGO.TabIndex = 2;
+            this.cmdGO.Text = "GO!";
+            this.cmdGO.UseVisualStyleBackColor = true;
+            this.cmdGO.Click += new System.EventHandler(this.cmdGO_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Request URI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Response:";
+            // 
+            // cboVerb
+            // 
+            this.cboVerb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboVerb.FormattingEnabled = true;
+            this.cboVerb.Items.AddRange(new object[] {
+            "GET",
+            "POST"});
+            this.cboVerb.Location = new System.Drawing.Point(450, 11);
+            this.cboVerb.Name = "cboVerb";
+            this.cboVerb.Size = new System.Drawing.Size(80, 21);
+            this.cboVerb.TabIndex = 11;
+            this.cboVerb.Text = "POST";
+            // 
+            // txtPOSTData
+            // 
+            this.txtPOSTData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPOSTData.Location = new System.Drawing.Point(91, 133);
+            this.txtPOSTData.Multiline = true;
+            this.txtPOSTData.Name = "txtPOSTData";
+            this.txtPOSTData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPOSTData.Size = new System.Drawing.Size(537, 75);
+            this.txtPOSTData.TabIndex = 12;
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmdClear.Location = new System.Drawing.Point(91, 323);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(75, 23);
+            this.cmdClear.TabIndex = 13;
+            this.cmdClear.Text = "Clear";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // cmdCopy
+            // 
+            this.cmdCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cmdCopy.Location = new System.Drawing.Point(173, 323);
+            this.cmdCopy.Name = "cmdCopy";
+            this.cmdCopy.Size = new System.Drawing.Size(75, 23);
+            this.cmdCopy.TabIndex = 14;
+            this.cmdCopy.Text = "Copy";
+            this.cmdCopy.UseVisualStyleBackColor = true;
+            this.cmdCopy.Click += new System.EventHandler(this.cmdCopy_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Post Data";
+            // 
+            // cboURIs
+            // 
+            this.cboURIs.FormattingEnabled = true;
+            this.cboURIs.Items.AddRange(new object[] {
+            "https://sapi.telstra.com/v1/oauth/token",
+            "https://tapi.telstra.com/v2/messages/provisioning/subscriptions",
+            "https://tapi.telstra.com/v2/messages/sms"});
+            this.cboURIs.Location = new System.Drawing.Point(91, 10);
+            this.cboURIs.Name = "cboURIs";
+            this.cboURIs.Size = new System.Drawing.Size(338, 21);
+            this.cboURIs.TabIndex = 16;
+            this.cboURIs.Text = "https://sapi.telstra.com/v1/oauth/token";
+            // 
+            // txtAuthToken
+            // 
+            this.txtAuthToken.Location = new System.Drawing.Point(91, 107);
+            this.txtAuthToken.Name = "txtAuthToken";
+            this.txtAuthToken.Size = new System.Drawing.Size(537, 20);
+            this.txtAuthToken.TabIndex = 17;
+            this.txtAuthToken.Text = "IC3OwAeoHNIUZ0G7LlNtANOQn4P2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Auth Token:";
+            // 
+            // txtClientSecret
+            // 
+            this.txtClientSecret.Location = new System.Drawing.Point(91, 81);
+            this.txtClientSecret.Name = "txtClientSecret";
+            this.txtClientSecret.Size = new System.Drawing.Size(537, 20);
+            this.txtClientSecret.TabIndex = 19;
+            this.txtClientSecret.Text = "CF1YbicH41Po7FN0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Client Secret:";
+            // 
+            // txtClientID
+            // 
+            this.txtClientID.Location = new System.Drawing.Point(91, 55);
+            this.txtClientID.Name = "txtClientID";
+            this.txtClientID.Size = new System.Drawing.Size(537, 20);
+            this.txtClientID.TabIndex = 21;
+            this.txtClientID.Text = "K012t7dqlQAIzNq3xMycNPACUKbunxcx";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Client ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 358);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtClientID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtClientSecret);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAuthToken);
+            this.Controls.Add(this.cboURIs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmdCopy);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.txtPOSTData);
+            this.Controls.Add(this.cboVerb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdGO);
+            this.Controls.Add(this.txtResponse);
+            this.MinimumSize = new System.Drawing.Size(442, 235);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "c# REST Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button cmdGO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboVerb;
+        private System.Windows.Forms.TextBox txtPOSTData;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.Button cmdCopy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboURIs;
+        private System.Windows.Forms.TextBox txtAuthToken;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtClientSecret;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtClientID;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ VP-15 Base Project/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VP_15_Base_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region UI Event Handlers
+
+        private void cmdGO_Click(object sender, EventArgs e)
+        {
+            RestClient rClient = new RestClient();
+            rClient.endPoint = cboURIs.Text;
+
+            switch(cboVerb.Text)
+            {
+                case "POST":
+                    rClient.httpMethod = httpVerb.POST;
+                    rClient.postData = txtPOSTData.Text;
+                    rClient.authToken = txtAuthToken.Text;
+                    rClient.clientID = txtClientID.Text;
+                    rClient.clientSecret = txtClientSecret.Text;
+
+                    break;
+                default:
+                    rClient.httpMethod = httpVerb.GET;
+                    break;
+            }
+
+
+
+            
+
+            debugOutput("REst Client Created");
+
+            string strResponse = string.Empty;
+
+            strResponse = rClient.makeRequest();
+
+            debugOutput(strResponse);
+        }
+
+        private void cmdClear_Click(object sender, EventArgs e)
+        {
+            txtResponse.Text = string.Empty;
+        }
+
+        private void cmdCopy_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(txtResponse.Text);
+        }
+
+        #endregion
+
+        private void debugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                txtResponse.Text = txtResponse.Text + strDebugText + Environment.NewLine;
+                txtResponse.SelectionStart = txtResponse.TextLength;
+                txtResponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message.ToString() + Environment.NewLine);
+            }
+        }
+
+       
+    }
+}
